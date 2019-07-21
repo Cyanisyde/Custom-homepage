baa19ec50793302c969366eca9428d91bc5009f6
Cyanisyde <Mikael.jafari@gmail.com>
2019-07-21 04:49:19 +0200

    Updates

---
+++ precache-manifest.988227373a5d48393c139c71b0a1c6b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44d2f13cef5aaed32c18c8c8b76e68d",
+    "url": "/custom-homepage/index.html"
+  },
+  {
+    "revision": "28fa60ca3b892feeb382",
+    "url": "/custom-homepage/static/css/main.5c64a8c2.chunk.css"
+  },
+  {
+    "revision": "ff9c0737502dd197d5c2",
+    "url": "/custom-homepage/static/js/2.d4f08238.chunk.js"
+  },
+  {
+    "revision": "28fa60ca3b892feeb382",
+    "url": "/custom-homepage/static/js/main.c2779bbf.chunk.js"
+  },
+  {
+    "revision": "b9546260d5e7d988cd60",
+    "url": "/custom-homepage/static/js/runtime~main.62f364c7.js"
+  }
+]);
